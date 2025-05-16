dec24080ebc7d356a2883b96676646e106c5c5e3
Tuscy <rhysesummers@gmail.com>
2025-05-16 16:18:54 +0100

    Added leads page

--- app/page.tsx
+++ app/page.tsx
@@ -110,7 +110,7 @@ export default function Home() {
             </CardContent>
           </Card>
 
-          <div className="mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 text-left">
+          {/* <div className="mt-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 text-left">
             {features.map((feature, index) => (
               <Card key={index} className="bg-popover">
                 <CardContent className="p-6">
@@ -124,7 +124,7 @@ export default function Home() {
                 </CardContent>
               </Card>
             ))}
-          </div>
+          </div> */}
         </div>
       </section>
 
