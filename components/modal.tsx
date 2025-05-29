c7aeef0b248570e4bb99d7760ece9f8409fb6c68
Tuscy <rhysesummers@gmail.com>
2025-05-29 17:22:11 +0100

    Fixed the popup issue

--- components/modal.tsx
+++ components/modal.tsx
@@ -53,20 +53,25 @@ export function Modal({ onClose }: { onClose: () => void }) {
     };
 
     return(
-        <div id="modal-bg" className="absolute left-0 top-0 flex flex-col bg-zinc-700/85 background-blur-sm h-dvh w-screen items-center justify-center" onClick={closeModalBgClick}>
+        <div id="modal-bg" className="fixed left-0 top-0 flex flex-col bg-zinc-700/90 background-blur-sm h-dvh w-screen items-center justify-center" onClick={closeModalBgClick}>
             <div className="flex flex-col bg-background relative rounded-lg p-4 m-4 shadow-2xl">
                 <button className="absolute right-5 hover:cursor-pointer" onClick={onClose}><X/></button>
-                <div className="flex flex-col items-center justify-center">
-                    <h1 className="text-2xl font-bold p-8">Join the Waitlist</h1>
+                <div className="flex flex-col items-center m-8 p-4">
+                    <h1 className="text-2xl font-bold pb-4">Join the Waitlist</h1>
+                    <p className="wrap text-center max-w-md mb-8">
+                        Put your email in the box below to sign up to the waitlist, 
+                        we will send you information about our progress 
+                        and you will be the first to know when the app is released.
+                    </p>
                     <form className="flex flex-col" onSubmit={handleWaitlist}>
                         <input 
                             placeholder="janedoe@gmail.com"
                             required
-                            className="text-center"
+                            className="rounded-sm text-primary-foreground text-center bg-primary p-1"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}                         
                         />
-                        <Button type="submit" className="mt-8 px-8 py-7 text-xl font-bold bg-primary hover:bg-primary/90 rounded-full shadow-lg transition transform focus:ring-4 focus:ring-primary/30">
+                        <Button type="submit" className="mt-10 px-8 py-7 text-xl font-bold bg-primary hover:bg-primary/90 rounded-full shadow-lg transition transform focus:ring-4 focus:ring-primary/30">
                             Join the Waitlist
                         </Button>
                     </form>
