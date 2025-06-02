05c89c4cfd121ac3d3e3492bff71252321f6810a
Tuscy <rhysesummers@gmail.com>
2025-06-02 15:58:28 +0100

    Styled the terms and privacy policy

--- app/terms/page.tsx
+++ app/terms/page.tsx
@@ -7,9 +7,9 @@ export default function PrivacyPolicy() {
       <Header/>
       <div className="flex flex-col items-center h-screen">
         <div className="max-w-md gap-2">
-          <h1>Terms of Service</h1>
+          <h1 className="text-bold text-center text-3xl mt-8">Terms of Service</h1>
 
-          <p>We are developing the app as you read this, make sure to check back here once it&apos;s live.</p>
+          <p className="mt-2 text-lg text-center">We are developing the app as you read this, check back here once it&apos;s live.</p>
         </div>
       </div>
       <Footer/>
