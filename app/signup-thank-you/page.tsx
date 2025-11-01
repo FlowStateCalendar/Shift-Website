ccd63ef96f68c9a79be23206d6e9b69e12324755
Tuscy <rhysesummers@gmail.com>
2025-11-01 17:45:12 +0000

    eslint fix

--- app/signup-thank-you/page.tsx
+++ app/signup-thank-you/page.tsx
@@ -11,7 +11,7 @@ export default function PrivacyPolicy() {
         <div className="max-w-md gap-2 pb-4">
           <h1 className="text-bold text-center text-3xl mt-8">Thank You!</h1>
 
-          <p className="mt-2 text-lg text-center">You have Successfully joined the waitlist for Shift Habits. Look out for our emails to see what's new.</p>
+          <p className="mt-2 text-lg text-center">You have Successfully joined the waitlist for Shift Habits. Look out for our emails to see what&apos;s new.</p>
         </div>
         <Link href="/">
             <Button className="align-center">Go Home</Button>
